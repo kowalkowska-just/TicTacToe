600d66ff1f8d1fc82df9650c15d0344748e03dc5
Justyna <kowalkowska.just@gmail.com>
2020-10-22 21:55:46 +0200

    Complete tic-tac-toe game.

--- KiK/KiKViewController.swift
+++ KiK/KiKViewController.swift
@@ -14,6 +14,7 @@ enum Symbol: String {
 }
 
 extension Symbol {
+    
     var oposite: Symbol {
         switch self {
         case .X:    return  .O
@@ -21,6 +22,16 @@ extension Symbol {
         case .none: return .none
         }
     }
+    
+    var isNone: Bool {
+        Symbol.none == self
+    }
+}
+
+enum GameStateResult {
+    case winner
+    case tie
+    case playing
 }
 
 
@@ -33,18 +44,67 @@ class KiKViewController: UIViewController {
     // [.none, .none, .none, ...]
     var model: [Symbol] = Array(repeating: .none, count: 9)
     
+    let winningLineIndexes = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        
+        [0,4,8],
+        [2,4,6]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupGame()
-        
     }
 
     @IBAction func didTapButton(_ sender: UIButton) {
         
-        sender.setTitle(currentSymbol.rawValue, for: .normal)
-        currentSymbol = currentSymbol.oposite
+        let tappedSymbol: Symbol = model[ sender.tag ]
         
+        guard tappedSymbol.isNone else { return }
+            
+        sender.setTitle(currentSymbol.rawValue, for: .normal)
+        model[ sender.tag ] = currentSymbol
+        
+        switch gameEnded() {
+            case .winner:   showWinner()
+            case .tie:      showTie()
+            case .playing:  break
+        }
+
+        currentSymbol = currentSymbol.oposite
+    }
+    
+    func showWinner() {
+        let ok = UIAlertAction(title: "New Game", style: .default) { _ in
+            self.setupGame()
+        }
+        
+        let alert = UIAlertController(title: "Geme won by: \(currentSymbol.rawValue)!",
+                                      message: nil,
+                                      preferredStyle: .alert)
+        
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showTie() {
+        let ok = UIAlertAction(title: "New Game", style: .default) { _ in
+            self.setupGame()
+        }
+        
+        let alert = UIAlertController(title: "No one won!",
+                                      message: nil,
+                                      preferredStyle: .alert)
+        
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
     }
     
     func setupGame() {
@@ -55,5 +115,47 @@ class KiKViewController: UIViewController {
         }
         
     }
+    
+    func gameEnded() -> GameStateResult {
+        
+        var linesOfSymbols: [ [Symbol] ] = []
+        
+        for line in winningLineIndexes {
+            let symbolLine = convertLinesToSymbols(line: line)
+            linesOfSymbols.append(symbolLine)
+        }
+        
+        for symbolLine in linesOfSymbols {
+            if checkIfAllSymbolsAreTheSame(symbolLine: symbolLine) {
+                return .winner
+            }
+        }
+        
+        if model.contains(.none) == false {
+            return .tie
+        }
+    
+        return .playing
+    }
+    
+    func checkIfAllSymbolsAreTheSame(symbolLine: [Symbol]) -> Bool {
+        if symbolLine.contains(.none) {
+            return false
+        }
+        
+        let symbolSet: Set<Symbol> = Set(symbolLine)
+        return symbolSet.count == 1
+    }
+    
+    func convertLinesToSymbols(line: [Int]) -> [Symbol] {
+        var symbols: [Symbol] = []
+        
+        for index in line {
+            symbols.append( model[index] )
+        }
+        
+        return symbols
+    }
+    
 }
 
