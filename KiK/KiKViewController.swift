961ae72d7be7e377c32faa793373c9df03d47bb7
Justyna <kowalkowska.just@gmail.com>
2020-10-22 20:55:01 +0200

    When the button is pressed, it will set the current symbol, O or X.

--- KiK/KiKViewController.swift
+++ KiK/KiKViewController.swift
@@ -7,19 +7,53 @@
 
 import UIKit
 
+enum Symbol: String {
+    case X      = "X"
+    case O      = "O"
+    case none   = "-"
+}
+
+extension Symbol {
+    var oposite: Symbol {
+        switch self {
+        case .X:    return  .O
+        case .O:    return .X
+        case .none: return .none
+        }
+    }
+}
+
+
 class KiKViewController: UIViewController {
 
     @IBOutlet var buttons: [UIButton]!
     
+    var currentSymbol: Symbol = .O
+    
+    // [.none, .none, .none, ...]
+    var model: [Symbol] = Array(repeating: .none, count: 9)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupGame()
+        
     }
 
     @IBAction func didTapButton(_ sender: UIButton) {
-        print("'\(sender.titleLabel?.text) was tapped.")
+        
+        sender.setTitle(currentSymbol.rawValue, for: .normal)
+        currentSymbol = currentSymbol.oposite
+        
     }
     
-
+    func setupGame() {
+        model = Array(repeating: .none, count: 9)
+        
+        for button in buttons {
+            button.setTitle(Symbol.none.rawValue, for: .normal)
+        }
+        
+    }
 }
 
