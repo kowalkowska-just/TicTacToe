2da308a6383281d5eb7a4c6cf22906a851610841
Justyna <kowalkowska.just@gmail.com>
2020-10-23 19:03:01 +0200

    Added a Unit Test.

--- KiKTests/KiKTests.swift
+++ KiKTests/KiKTests.swift
@@ -10,24 +10,64 @@ import XCTest
 
 class KiKTests: XCTestCase {
 
+    //system under test
+    var sut: KiKViewController!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = KiKViewController()
+        
+        
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut.model = Array(repeating: .none, count: 9)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_checkIfAllSymbolsAreTheSame() throws {
+
+        let symbols: [Symbol] = Array(repeating: .X, count: 3)
+        XCTAssert(sut.checkIfAllSymbolsAreTheSame(symbolLine: symbols))
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_checkIfAllSymbolsAreTheSame_ShoudReturnFlaseIfContainsNone() throws {
+        
+        let symbols: [Symbol] = [.none, .O, .X]
+        XCTAssertFalse(sut.checkIfAllSymbolsAreTheSame(symbolLine: symbols))
+    }
+    
+    func test_checkIfEndGameReturnWin() {
+        sut.model = Array(repeating: .X, count: 9)
+        XCTAssertEqual(sut.gameEnded(), GameStateResult.winner)
+    }
+
+    func test_checkIfEndGameReturnTie() {
+        sut.model = [ .O, .X, .O,
+                      .X, .O, .X,
+                      .X, .O, .X]
+        XCTAssertEqual(sut.gameEnded(), GameStateResult.tie)
+    }
+    
+    func test_checkIfEndGameReturnPlaying() {
+        sut.model = [.none, .X, .O,
+                     .X, .O, .X,
+                     .X, .O, .O]
+        XCTAssertEqual(sut.gameEnded(), GameStateResult.playing)
+    }
+    
+    func test_checkIfLinesAreConvertedToSymbol() {
+        sut.model = [.O, .O, .O,
+                     .X, .X, .X,
+                     .none, .none, .none]
+        
+        XCTAssertEqual(sut.convertLinesToSymbols(line: [0, 1, 2]),
+                                                       [.O, .O, .O])
+        XCTAssertEqual(sut.convertLinesToSymbols(line: [3 , 4, 5]),
+                                                       [.X , .X, .X])
+        XCTAssertEqual(sut.convertLinesToSymbols(line: [6, 7, 8]),
+                                                       [.none, .none, .none])
+        XCTAssertEqual(sut.convertLinesToSymbols(line: [1, 3, 6]),
+                                                       [.O, .X, .none])
     }
 
 }
